bb77812372b5e0ccba59a4b473ac7767c952c937
Zurg <56041372+C-Zuge@users.noreply.github.com>
2019-10-01 17:37:03 -0300

    Update Server.py

--- RST/Server.py
+++ RST/Server.py
@@ -1,50 +1,109 @@
-##### SOCKET CLIENT
+########## SOCKET SERVER
 
-import sys
 import socket
+import sys
 import time
-import threading
-  
+import math
+import threading 
+import requests
 
-print('Digite o IP do servidor')
-IP = input()
 
-print('Entre com a porta do Servidor')
+ip, porta = input("Entre com o IP da API: "), int(input("Entre com a porta da API: "))
+
+API_URL = f"http://{ip}:{porta}/vpp"
+
+        
+class Trafo:
+    def __init__(self, tId, serie, tensaoRef, error):
+        self.tId = tId
+        self.serie = serie
+        self.tensaoRef = tensaoRef
+        self.error = error
+        self.tensions = []
+
+    def add_voltage(self, t):
+        self.tensions.append(t)
+
+    def media(self):
+        return sum(self.tensions) / len(self.tensions)
+
+    def is_approved(self, t):
+        error = (self.tensaoRef - t) / (self.tensaoRef)
+        return [error, "aprovado"] if error < (self.error / 100) else [error, "reprovado"]
+
+    def get_list(self, Vpp):
+        error, status = self.is_approved(Vpp)
+        media = self.media()
+
+        return {
+            "tId" : self.tId ,
+            "serie" : self.serie ,
+            "tensaoRef" : self.tensaoRef ,
+            "errorRef" : self.error ,
+            "error": error,
+            "status": status,
+            "media": media
+        }
+
+
+
+def write(vpp):
+    try:
+        requests.post(API_URL, vpp)
+    except Exception as e:
+        print("deu ruim pra enviar pro sv", e)
+
+
+def RecebeDado(conn,addr):
+    print('A thread', addr, 'iniciou')
+    ID = str(conn.recv(10),'utf-8')
+    print('O Transformador ', ID, ' registrado no socket')
+    
+    Nserie = int(conn.recv(10))
+    VoltageRMS = float(conn.recv(10))
+    Erro = float(conn.recv(10))
+
+    print('---', ID, Nserie, VoltageRMS, Erro)
+    n=0
+
+    t1 = Trafo(ID,Nserie,VoltageRMS,Erro)
+
+    while n != -1:
+        Vpp = int(conn.recv(10))
+        print('Recebi')
+        t1.add_voltage(Vpp)
+
+        if Vpp == int(0) :
+            print('Encerrei')
+            break
+        else:
+            wr = t1.get_list(Vpp)
+            print("Sending", wr, "to the server")
+            write(wr)
+            
+
+
+
+print('Digite um Numero de Porta')
 Port = int(input())
 
-print('Entre com o ID do Cliente')
-ID = input()
-
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # AF_INET = IPv4 SOCK_STREAM = TCP
 
 try:
-    sock.connect((IP,Port))
-    sock.send(bytes(ID,'utf-8'))
+    sock.bind(('0.0.0.0',Port))
 except:
     print('### Bind Error, closing session ###')
     time.sleep(2)
     sys.exit()
 
-print('Qual e Numero do Transformador?')
-N_serie = input()
-sock.send(bytes(N_serie,'utf-8'))
+sock.listen(2)
+print('Aguardando conexoes em: ',Port)
 
-print('Qual e a Tensao rms esperada?')
-V=input()
-sock.send(bytes(V,'utf-8'))
-
-print('Qual e o erro esperado? (em %)')
-erro=input()
-sock.send(bytes(erro,'utf-8'))
-
-while True:
-    n=0
-    time.sleep(2)
-    while True:
-        print('Digite a Tensão Pico a Pico')
-        tensao = input()
-        sock.send(bytes(tensao,'utf-8'))
-       
-        
-       # n=-1
-        #sock.send(bytes(n,'utf-8'))
+while True :
+    conn, addr = sock.accept()
+    print('Conexao estabelecida com: ',addr)
+    t = threading.Thread(target=RecebeDado,args=(conn,addr))
+    t.start()
+    
+print('A Sessao com: ',addr,' foi encerrada')
+sock.close()
